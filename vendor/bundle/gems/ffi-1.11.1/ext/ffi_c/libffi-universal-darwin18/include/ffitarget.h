60f3d59f320ebc4930b4d2acaececac65276e51f
auhnix <hava.xinhua@gmail.com>
2019-06-29 22:45:18 -0400

    fixing symlink

--- vendor/bundle/gems/ffi-1.11.1/ext/ffi_c/libffi-universal-darwin18/include/ffitarget.h
+++ vendor/bundle/gems/ffi-1.11.1/ext/ffi_c/libffi-universal-darwin18/include/ffitarget.h
@@ -1,1 +1,47 @@
-/Library/Ruby/Gems/2.3.0/gems/ffi-1.11.1/ext/ffi_c/libffi/src/x86/ffitarget.h
+Last login: Sat Jun 29 22:16:35 on ttys001
+ganymede:~ havaxp$ sudo vim /Library/Ruby/Gems/2.3.0/gems/ffi-1.11.1/ext/ffi_c/libffi/src/x86/ffitarget.h
+Password:
+ganymede:~ havaxp$ sudo nano /Library/Ruby/Gems/2.3.0/gems/ffi-1.11.1/ext/ffi_c/libffi/src/x86/ffitarget.h
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  GNU nano 2.0.6 File: ...xt/ffi_c/libffi/src/x86/ffitarget.h           
+
+/* -----------------------------------------------------------------*-C$
+   ffitarget.h - Copyright (c) 2012, 2014, 2018  Anthony Green
+                 Copyright (c) 1996-2003, 2010  Red Hat, Inc.
+                 Copyright (C) 2008  Free Software Foundation, Inc.
+
+   Target configuration macros for x86 and x86-64.
+
+   Permission is hereby granted, free of charge, to any person obtaining
+   a copy of this software and associated documentation files (the
+   ``Software''), to deal in the Software without restriction, including
+   without limitation the rights to use, copy, modify, merge, publish,
+   distribute, sublicense, and/or sell copies of the Software, and to
+   permit persons to whom the Software is furnished to do so, subject to
+   the following conditions:
+
+   The above copyright notice and this permission notice shall be inclu$
+   in all copies or substantial portions of the Software.
+
+   THE SOFTWARE IS PROVIDED ``AS IS'', WITHOUT WARRANTY OF ANY KIND,
+                           [ Read 147 lines ]
+^G Get Help ^O WriteOut ^R Read File^Y Prev Page^K Cut Text ^C Cur Pos
+^X Exit     ^J Justify  ^W Where Is ^V Next Page^U UnCut Tex^T To Spell
+
